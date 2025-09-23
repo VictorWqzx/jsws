64765e83117dff5f8247dded286ef9bd56c1f7c2
VictorWqzx <workgruop038@gmail.com>
2025-09-23 10:40:28 +0300

    исправление полиндрома

--- tasks/basic-js/task_19_is_palindrome.js
+++ tasks/basic-js/task_19_is_palindrome.js
@@ -14,16 +14,6 @@ const isPalindrome = (str) => {
   return true;
 };
 
-console.log(isPalindrome("racecar")); 
-console.log(isPalindrome("hello"));  
-
-function isPolindrom(str){ //str.split('') все равно создает массив, я говорила про метод с указателями
- return str === str.split('').reverse().join('');
-}
-console.log(isPolindrom("racecar"));
-console.log(isPolindrom("hello"));
-
-
 function isPalindrome(str) {
   let left = 0;
   let right = str.length - 1;
@@ -38,5 +28,17 @@ function isPalindrome(str) {
   return true;
 }
 
-console.log(isPalindrome("racecar"));
+console.log(isPalindrome("racecar")); 
+console.log(isPalindrome("hello"));   
+
+console.log(isPalindrome("racecar")); 
 console.log(isPalindrome("hello"));  
+
+function isPolindrom(str){ //str.split('') все равно создает массив, я говорила про метод с указателями
+ return str === str.split('').reverse().join('');
+}
+console.log(isPolindrom("racecar"));
+console.log(isPolindrom("hello"));
+
+
+
