4159d1314f711d85f02bab73d670574296f7bd92
VictorWqzx <workgruop038@gmail.com>
2025-09-24 12:11:25 +0300

    карирование и рекурсия

--- tasks/advanced-js/task_28_sum.js
+++ tasks/advanced-js/task_28_sum.js
@@ -4,3 +4,16 @@
 пока не будет вызвана без аргументов - 
 тогда возвращается сумма переданных ранее чисел (sum(2)(5)(9)())
 */
+function sum(num) {
+    let currentSum = num || 0;
+    const inner = (nextNum) => {
+        if (nextNum === undefined) {
+            return currentSum;
+        }
+        currentSum += nextNum;
+        return inner;
+    };
+    return inner;
+}
+console.log(sum(2)(5)(9)());
+console.log(sum(1)(2)(3)(4)(5)());
--- tasks/advanced-js/task_31_flatten_tree.js
+++ tasks/advanced-js/task_31_flatten_tree.js
@@ -19,3 +19,28 @@ const tree = {
 Результат: flattenTree(tree) → [1, 2, 3, 4, 5]
                         
 */
+function flattenTree(tree) {
+  let result = [];
+  function traverse(node) {
+    result.push(node.value);
+    for (let child of node.children) {
+      traverse(child);
+    }
+  }
+  traverse(tree);
+  return result;
+}
+const tree = {
+  value: 1,
+  children: [
+    { value: 2, children: [] },
+    { 
+      value: 3, 
+      children: [
+        { value: 4, children: [] },
+        { value: 5, children: [] }
+      ] 
+    }
+  ]
+};
+console.log(flattenTree(tree));
