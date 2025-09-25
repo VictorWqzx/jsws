0bdf71a5da831f1b79e7672220bacd6bd482cc0b
VictorWqzx <workgruop038@gmail.com>
2025-09-25 14:01:40 +0300

    this и контекст,структуры данных

--- tasks/advanced-js/task_38_my_bind.js
+++ tasks/advanced-js/task_38_my_bind.js
@@ -9,3 +9,16 @@ const person = { name: 'Alice' };
 const boundGreet = greet.myBind(person, 'Hello');
 boundGreet(); // Output: Alice says: Hello
 */
+function myBind(fn, context, ...args) {
+  return function(...restArgs) {
+    return fn.apply(context, [...args, ...restArgs]);
+  };
+}
+
+function greet(message) {
+  console.log(this.name + ' says: ' + message);
+}
+
+const person = { name: 'Alice' };
+const boundGreet = myBind(greet, person, 'Hello');
+boundGreet();
