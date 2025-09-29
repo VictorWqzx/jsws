28905a9f6e39bf00191b6bcdcd754cef89aba87c
VictorWqzx <workgruop038@gmail.com>
2025-09-29 12:22:11 +0300

    углубленная практика 51,53,56

--- tasks/async-js/task_53_invert.js
+++ tasks/async-js/task_53_invert.js
@@ -3,3 +3,14 @@
 Если значения повторяются, сохранить массив ключей:
 invert({ a: 1, b: 2, c: 1 }) // → { 1: ['a','c'], 2: ['b'] }
 */
+function invert(obj) {
+    return Object.fromEntries(Object.entries(obj).map(([Key, Value]) => [Value, Key]));
+}
+console.log(invert({ a: 1, b: 2, c: 1 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3, f: 4 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3, f: 4, g: 5 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3, f: 4, g: 5, h: 6 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3, f: 4, g: 5, h: 6, i: 7 }));
+console.log(invert({ a: 1, b: 2, c: 1, d: 2, e: 3, f: 4, g: 5, h: 6, i: 7, j: 8 }));
--- tasks/async-js/task_56.js
+++ tasks/async-js/task_56.js
@@ -2,3 +2,12 @@
 Реализуйте функцию deepClone(obj), которая создаёт глубокую копию переданного объекта. 
 Копия не должна иметь общих ссылок с оригиналом (кроме примитивов)
 */
+function deepClone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+console.log(deepClone({ a: 1, b: 2, c: 3 }));
+console.log(deepClone({ a: 1, b: 2, c: 3, d: 4 }));
+console.log(deepClone({ a: 1, b: 2, c: 3, d: 4, e: 5 }));
+console.log(deepClone({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 }));
+console.log(deepClone({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7 }));
+console.log(deepClone({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8 }));
