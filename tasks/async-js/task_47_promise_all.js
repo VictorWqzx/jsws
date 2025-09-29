f7342035b31cd5ed42adaba0dd9430a8fe871dcf
VictorWqzx <workgruop038@gmail.com>
2025-09-29 11:39:24 +0300

    асинхронность

--- tasks/async-js/task_47_promise_all.js
+++ tasks/async-js/task_47_promise_all.js
@@ -1,3 +1,43 @@
 /*
 Написать Promise.all полифил
 */
+// в ручную с .then .catch
+function promiseAll(promises) {
+    return new Promise((resolve, reject) => {
+        if (promises.length === 0) return resolve([]);
+
+        const result = [];
+        let completed = 0;
+
+        promises.forEach((promise, index) => {
+            Promise.resolve(promise)
+                .then(value => {
+                    result[index] = value;
+                    completed++;
+                    if (completed === promises.length) resolve(result);
+                })
+                .catch(reject);
+        });
+    });
+}
+// c await
+promiseAll([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
+    .then(console.log)
+    .catch(console.error);
+
+    async function promiseAll(promises) {
+        promises = promises.map(p => Promise.resolve(p));
+    
+        const results = [];
+        for (let i = 0; i < promises.length; i++) {
+            results.push(await promises[i]);
+        }
+        return results;
+    }
+    
+    promiseAll([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
+        .then(console.log)
+        .catch(console.error);
+    
+
+
--- tasks/async-js/task_48_delay_promise.js
+++ tasks/async-js/task_48_delay_promise.js
@@ -2,3 +2,7 @@
 Реализуйте функцию delayPromise(delay, value), которая возвращает промис, 
 резолвящийся через указанное количество миллисекунд со значением value.
 */
+function delayPromise(delay, value) {
+    return new Promise(resolve => setTimeout(resolve, delay, value));
+}
+console.log(delayPromise(1000, 'Hello'));
